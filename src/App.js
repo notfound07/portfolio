d7dc43a461438efe3de00bf802ac5e14f6748524
Aman Bhatt <22680764+notfound07@users.noreply.github.com>
2024-08-23 23:59:56 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
